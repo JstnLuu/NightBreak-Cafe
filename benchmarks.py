90beea2b12ded762f54ab9609bb08d677a154972
Leica Yasukawa <q2v8x4yptw@privaterelay.appleid.com>
2026-04-13 12:47:52 -0700

    Update benchmarks.py

--- benchmarks.py
+++ benchmarks.py
@@ -1,8 +1,8 @@
 import time
 
-from system import HashTable
 from system import NightbreakCafeSystem
 from system import SLLQueue
+from system import binary_search_first
 from system import merge_sort
 
 
@@ -29,11 +29,33 @@ def run_benchmarks():
     end = time.time()
     results["50_order_workflow_seconds"] = round(end - start, 6)
 
-    hash_table = HashTable(101)
+    queue = SLLQueue()
+    start = time.time()
     for number in range(1000):
-        hash_table.set("order-" + str(number), number)
+        queue.enqueue(number)
+    for number in range(1000):
+        queue.dequeue()
+    end = time.time()
+    results["1000_queue_ops_seconds"] = round(end - start, 6)
+
+    values = list(range(1000, 0, -1))
+    start = time.time()
+    merge_sort(values)
+    end = time.time()
+    results["merge_sort_1000_values_seconds"] = round(end - start, 6)
 
     start = time.time()
+    binary_search_first(values, 750)
+    end = time.time()
+    results["binary_search_seconds"] = round(end - start, 6)
+
+    return results
+
+
+if __name__ == "__main__":
+    benchmark_results = run_benchmarks()
+    for key in benchmark_results:
+        print(key + ": " + str(benchmark_results[key]))
     for number in range(1000):
         hash_table.get("order-" + str(number))
     end = time.time()
